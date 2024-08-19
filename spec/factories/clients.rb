f8d7d7a029a9b809272b295bddd69946c1904053
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-19 13:17:12 +0900

    Add validation tests for Client model using FactoryBot

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -2,5 +2,10 @@
 
 FactoryBot.define do
   factory :client do
+    sequence(:name) { |n| "ClientTester#{n}" }
+    sequence(:email) { |n| "tester#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
+
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Client do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(create(:client)).to be_valid
+  end
 end
