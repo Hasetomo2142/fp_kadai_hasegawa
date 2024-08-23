de9a3d37533377744a4c766c03f1286c550d9767
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-23 17:51:32 +0900

    seed test data randam

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-# Plannerとその空き枠を作成する
 50.times do |i|
   planner = Planner.create!(
     name: "Planner#{i}",
@@ -7,12 +6,20 @@
     password: 'password',
     password_confirmation: 'password'
   )
-  10.times do |j|
-    start_time = Time.current.to_date + 10.hours + j*30.minutes
+
+  20.times do |j|
+    # 今日から一週間以内のランダムな日を取得
+    random_day = rand(0..6).days.from_now.to_date
+    
+    # 10時から18時までの30分刻みでランダムな時間を取得
+    random_hour = rand(10..17) # 10時から17時
+    random_minute = rand(0..1) * 30 # 0 または 30 分
+    
+    start_time = random_day + random_hour.hours + random_minute.minutes
+
     planner.meetings.create!(
       start_time: start_time,
       end_time: start_time + 30.minutes,
     )
   end
 end
-
