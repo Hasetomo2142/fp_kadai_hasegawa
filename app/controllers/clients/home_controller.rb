b645f015facef005d5b5542aebe4d999a5359eb6
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-23 13:07:09 +0900

    fetch current_meetings from DB

--- app/controllers/clients/home_controller.rb
+++ app/controllers/clients/home_controller.rb
@@ -4,6 +4,8 @@ module Clients
   class HomeController < ApplicationController
     before_action :authenticate_client!
     def home
+      @meetings = Meeting.fetch_previous_and_next_three_month(Time.now)
+      @is_client = true
       render 'clients/home'
     end
   end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Meeting < ApplicationRecord
+  # attr_accessor :client_id, :planner_id, start_time, end_time
+
   belongs_to :client, optional: true
   belongs_to :planner
+
+
+  class << self
+
+    def fetch_previous_and_next_three_month(current_date)
+      previous_month = current_date.ago(3.months)
+      next_month = current_date.since(3.months)
+      @current_meetings ||= begin
+        Meeting.where(start_time: previous_month..next_month)
+      end
+    end
+  end
 end
