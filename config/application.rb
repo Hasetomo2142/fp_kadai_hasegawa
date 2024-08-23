681d27dc8644cc09b4af52db72f5294ad58d90a2
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-23 21:34:07 +0900

    set time zone Tokyo

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.time_zone = "Tokyo"
     config.load_defaults 7.2
     config.autoload_lib(ignore: %w[assets tasks])
     config.generators.system_tests = nil
