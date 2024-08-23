e26bf523c8c6a0bae36680412494f18d43c98191
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-23 15:09:08 +0900

    deleted duplicate hash

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,6 +9,5 @@ class Client < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email,
             format: { with: Devise.email_regexp },
-            uniqueness: true,
             uniqueness: { case_sensitive: true }
 end
