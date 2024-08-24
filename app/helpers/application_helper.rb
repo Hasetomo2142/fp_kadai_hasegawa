4b9146a2a185ec9a3305fe11f71350b8ed9e6311
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-24 14:52:06 +0900

    change formate search result view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,14 @@ module ApplicationHelper
     Time.zone.local(date.year, date.month, date.day, hour, minute)
   end
 
+  # {"date"=>"2024-08-27", "start_time"=>"10:00", "end_time"=>"18:00"}から日本語のメッセージを生成
+  def convert_to_message(range)
+    date = I18n.l(range[:date].to_date, format: :long)
+    start_time = range[:start_time]
+    end_time = range[:end_time]
+    "#{date}の#{start_time}から#{end_time}までの予定"
+  end
+
   def convert_empty_slot_to_symbol(empty_slot)
     return '○' if empty_slot >= 6
     return '△' if empty_slot >= 3
