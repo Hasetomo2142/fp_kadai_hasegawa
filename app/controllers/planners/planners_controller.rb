e15b14ef312818d75db927a3c6d50e01fbdbba6f
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-25 17:58:42 +0900

    fixed search planner

--- app/controllers/planners/planners_controller.rb
+++ app/controllers/planners/planners_controller.rb
@@ -2,10 +2,26 @@
 
 module Planners
   class PlannersController < ApplicationController
-    before_action :authenticate_planner!
+    before_action :authenticate_client!
 
-    def show
-      @planner = Planner.find(params[:id])
+    # def show
+    #   @planner = Planner.find(params[:id])
+    # end
+
+    def search
+      @planners = Planner.all
+
+      if params[:name].present? && params[:keyword].present?
+        @planners = @planners.where("name LIKE ? AND (name LIKE ? OR description LIKE ?)", 
+                                    "%#{params[:name]}%", "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+      elsif params[:name].present?
+        @planners = @planners.where("name LIKE ?", "%#{params[:name]}%")
+      elsif params[:keyword].present?
+        @planners = @planners.where("name LIKE ? OR description LIKE ?", "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+      end
+
+      @planners = @planners.page(params[:page]).per(5)
+      render 'planners/search'
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     registrations: 'planners/registrations'
   }
   resources :planners, controller: 'planners/planners', only: %i[index show]
-  get "planners/search" => "planners/search#search", as: :planners_search
+  get "search/planners" => "planners/planners#search", as: :planners_search
 
   devise_for :clients, controllers: {
     sessions:      'clients/sessions',
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get "clients/home" => "clients/home#home", as: :clients_home
 
   resources :meetings, controller: 'meetings/meetings', only: %i[new create edit update destroy]
-  get "meetings/search" => "meetings/meetings#search", as: :meetings_search
+  get "search/meetings" => "meetings/meetings#search", as: :meetings_search
 
   root "static_pages#home"
   get "static_pages/help"
