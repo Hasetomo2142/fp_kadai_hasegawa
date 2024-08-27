ca7c24412f77d9e16b9f7736d5d07957223b7ee0
Hasetomo2142 <kyutech.hasegawa@gmail.com>
2024-08-27 13:08:47 +0900

    create home view for planner and set routes

--- app/controllers/planners/registrations_controller.rb
+++ app/controllers/planners/registrations_controller.rb
@@ -52,9 +52,9 @@ module Planners
     # end
 
     # The path used after sign up.
-    # def after_sign_up_path_for(resource)
-    #   super(resource)
-    # end
+    def after_sign_up_path_for(resource)
+      planners_home_path
+    end
 
     # The path used after sign up for inactive accounts.
     # def after_inactive_sign_up_path_for(resource)
